b17de1917dbc1290aef10f6b50cd116719ce8ff6
Sproket <sproket176@gmail.com>
2018-11-18 16:16:07 +0100

    main

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const prefix = '/';
         let channel = guild.channels.get(info_channel);
         if(channel)
 channel.send(`Nowy kierowca, ${member}.`);
-member.send(`Siema ${member}. Przed rozpoczęciem rozgrywki jako taksówkarz zapoznaj się z kanałem **#faq**. Zawarłem tam najważniejsze informacje dotyczące firmy. Jeśli chcesz to dla własnej wygody możesz zainstalować modyfikacje z kanału **#nasze_modyfikacje**. Do zobaczenia w grze!`);
+member.send(`Siema ${member}. Przed rozpoczęciem rozgrywki jako taksówkarz zapoznaj się z kanałem **#faq**. Ustaw nickname (komenda /nick na dowolnym kanale) według wzoru **nick IC || nick OOC** oraz dodaj swój numer konta bankowego na kanał #konta_bankowe jeśli chcesz w przyszłości otrzymywać premie. Dla własnej wygody możesz zainstalować modyfikacje z kanału **#nasze_modyfikacje**. Do zobaczenia w grze!`);
 }
 });
 bot.on('guildMemberRemove', member => {
@@ -29,7 +29,7 @@ bot.on('guildMemberRemove', member => {
     if(guild) {
         let channel = guild.channels.get(info_channel);
         if(channel)
-            channel.send(member.displayName + " został wyproszony z serwera.");
+            channel.send(member.displayName + " został wyrzucony z firmy.");
 }
 });
 
@@ -69,26 +69,26 @@ if(cmd === '/panel') {
         var send = obj.list.slice(1).map(e=>'\n' + e.name) + '.';
         var count = obj.list.slice(1).length;
         message.delete().catch();
-        message.channel.send('**Lista osób pod panelem w grze (' + count + '):**' + send);
+        message.channel.send('**Lista graczy w panelu (' + count + '):**' + send);
     });
     return;
 }
 
-if(cmd === '/admins') {
-    request('http://cabs.5v.pl/bot/admins.php', function (error, res, body) {
-        var obj = JSON.parse(body);
-        var send = obj.admins.map(e=>'\n**' + e.name + '** na ' + e.rank) + '.';
-        var count = obj.admins.length;
-        message.delete().catch();
-        message.channel.send('Administracja online (' + count + '):' + send);
-    });
-    return;
-}
+//if(cmd === '/admins') {
+  //  request('http://cabs.5v.pl/bot/admins.php', function (error, res, body) {
+     //   var obj = JSON.parse(body);
+      //  var send = obj.admins.map(e=>'\n**' + e.name + '** na ' + e.rank) + '.';
+     //   var count = obj.admins.length;
+     //   message.delete().catch();
+     //   message.channel.send('Administracja online (' + count + '):' + send);
+    //});
+  //  return;
+//}
 
 if(cmd === '/help') {
             let botmessage = args.join(" ");
             message.delete().catch();
-            message.channel.send('```Komendy ogólnodostępne:\n/panel\n/admins```'); 
+            message.channel.send('```Komendy ogólnodostępne:\n/panel\n/admins (w budowie, ostatnie poprawki)```'); 
         return;
         }
 
