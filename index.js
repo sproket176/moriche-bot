57a749ff3f4083d5cbb5655a68dbed2346fcb93f
sproket176 <sproket176@gmail.com>
2018-12-23 12:25:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const server_id = '469152950669934593';
 const spam_channel = '469153165967884299';
 const info_channel = '469153199819849743';
 const cmd_channel = '473938199153213461';
+const count_channel = '526357805159022602';
 const prefix = '/';
 
   bot.on('ready', async () => {
@@ -20,17 +21,22 @@ const prefix = '/';
     if(guild) {
         let channel = guild.channels.get(info_channel);
         if(channel)
-channel.send(`Nowy kierowca, ${member}.`);
-member.send(`Siema ${member}. Przed rozpoczęciem rozgrywki jako taksówkarz zapoznaj się z kanałem **#faq**. Ustaw nickname (komenda /nick na dowolnym kanale) według wzoru **nick IC || nick OOC** oraz dodaj swój numer konta bankowego na kanał #konta_bankowe jeśli chcesz w przyszłości otrzymywać premie. Dla własnej wygody możesz zainstalować modyfikacje z kanału **#nasze_modyfikacje**. Do zobaczenia w grze!`);
-}
-});
+    channel.send(`Nowy kierowca, ${member}.`);
+    member.send(`Siema ${member}. Przed rozpoczęciem rozgrywki jako taksówkarz zapoznaj się z kanałem **#faq**. Ustaw nickname (komenda /nick na dowolnym kanale) według wzoru **nick IC || nick OOC** oraz dodaj swój numer konta bankowego na kanał #konta_bankowe jeśli chcesz w przyszłości otrzymywać premie. Dla własnej wygody możesz zainstalować modyfikacje z kanału **#nasze_modyfikacje**. Do zobaczenia w grze!`);
+    }
+    });
+
 bot.on('guildMemberRemove', member => {
     let guild = bot.guilds.get(server_id);
     if(guild) {
         let channel = guild.channels.get(info_channel);
         if(channel)
             channel.send(member.displayName + " został wyrzucony z firmy.");
-}
+
+        let count = guild.channels.get(count_channel);
+        if(count)
+            count.edit({ name: 'Wszystkich na konferencji:' });
+    }
 });
 
 bot.on('message', async message => {
@@ -44,8 +50,7 @@ bot.on('message', async message => {
             let botmessage = args.join(" ");
             message.delete().catch();
             var channel = bot.channels.get(spam_channel);
-            channel.send(botmessage);
-            
+            channel.send(botmessage);   
         }
         return;
     }
@@ -56,8 +61,7 @@ if(cmd === '/info') {
         let botmessage = args.join(" ");
         message.delete().catch();
         var channel = bot.channels.get(info_channel);
-        channel.send(botmessage);
-        
+        channel.send(botmessage);   
     }
     return;
 }
