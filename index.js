5fb79afad03e7d317ce9168f502e3c365b58a484
sproket176 <sproket176@gmail.com>
2019-03-18 15:19:36 +0100

    wrkshop & cabs on one host

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ const bot = new Discord.Client();
 
 const server_id = '469152950669934593';
 const spam_channel = '469153165967884299';
-const info_channel = '469153199819849743';
+const migrations_channel = '557199077666717698';
 const cmd_channel = '473938199153213461';
 const count_channel = '526357805159022602';
 const count_bots_channel = '526364100670914570';
-//const count_online_channel = '473938199153213461';
 const prefix = '/';
 
 function checkBots(guild) {
@@ -21,7 +20,7 @@ function checkBots(guild) {
 
   bot.on('ready', async () => {
     console.log(`Logged in as ${bot.user.tag}!`);
-    bot.user.setActivity('wpisz: /help', { type: 'PLAYING' });
+    bot.user.setActivity('Los Santos Role Play', { type: 'PLAYING' });
    });
   
   bot.on('guildMemberAdd', member => {
@@ -29,15 +28,15 @@ function checkBots(guild) {
     member.addRole(role).catch(console.error);
     let guild = bot.guilds.get(server_id);
     if(guild) {
-        let channel = guild.channels.get(info_channel);
+        let channel = guild.channels.get(migrations_channel);
         if(channel)
-    channel.send(`Nowy taksówkarz, ${member}.`);
-    member.send(`Siema ${member}. Przed rozpoczęciem rozgrywki jako taksówkarz zapoznaj się z kanałem **#faq**. Ustaw nickname (komenda /nick na dowolnym kanale) według wzoru **nick IC || nick OOC** oraz dodaj swój numer konta bankowego na kanał #konta_bankowe jeśli chcesz w przyszłości otrzymywać premie. Dla własnej wygody możesz zainstalować modyfikacje z kanału **#nasze_modyfikacje**. Do zobaczenia w grze!`);
+    channel.send(`Nowy pracownik, ${member}.`);
+    member.send(`Siema ${member}. Złożyłeś pomyślnie podanie i dołączyłeś do naszej poczekalni Discord. Pierwsze co musisz zrobić to ustawić nick według wzoru (komenda /nick na dowolnym kanale lub RMB -> zmień pseudonim): **nick IC || nick OOC** oraz dla własnej wygody możesz zainstalować modyfikacje z kanału **#nasze_modyfikacje**. Oczekuj na nadanie uprawnień przez osoby uprawnione w zależności od firmy, do której aplikowałeś.`);
 
     let count_all = guild.channels.get(count_channel);
     let count_bots = guild.channels.get(count_bots_channel);
     if(count_all)
-        count_all.edit({ name: `Członków: ${guild.memberCount}` });
+        count_all.edit({ name: `Pracowników: ${guild.memberCount}` });
     if(count_bots)
         count_bots.edit({ name: 'w tym botów: ' + checkBots(guild) });
     }
@@ -46,14 +45,14 @@ function checkBots(guild) {
 bot.on('guildMemberRemove', member => {
     let guild = bot.guilds.get(server_id);
     if(guild) {
-        let channel = guild.channels.get(info_channel);
+        let channel = guild.channels.get(migrations_channel);
         if(channel)
-            channel.send(member.displayName + " został wyrzucony z firmy.");
+            channel.send(member.displayName + " został wyrzucony z korporacji.");
 
     let count_all = guild.channels.get(count_channel);
     let count_bots = guild.channels.get(count_bots_channel);
     if(count_all)
-        count_all.edit({ name: `Członków: ${guild.memberCount}` });
+        count_all.edit({ name: `Pracowników: ${guild.memberCount}` });
     if(count_bots)
         count_bots.edit({ name: 'w tym botów: ' + checkBots(guild) });
     }
@@ -64,7 +63,7 @@ bot.on('message', async message => {
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
 
-    if(cmd === '/say') {
+    if(cmd === '/sayt') {
         if (message.channel.id === cmd_channel) {
         if(message.member.hasPermission("MANAGE_MESSAGES")) {
             let botmessage = args.join(" ");
@@ -75,35 +74,6 @@ bot.on('message', async message => {
         return;
     }
 }
-if(cmd === '/info') {
-    if (message.channel.id === cmd_channel) {
-    if(message.member.hasPermission("MANAGE_MESSAGES")) {
-        let botmessage = args.join(" ");
-        message.delete().catch();
-        var channel = bot.channels.get(info_channel);
-        channel.send(botmessage);   
-    }
-    return;
-}
-}
-
-if(cmd === '/panel') {
-    request('http://cabs.5v.pl/bot/', function (error, res, body) {
-        var obj = JSON.parse(body);
-        var send = obj.list.slice(1).map(e=>'\n' + e.name) + '.';
-        var count = obj.list.slice(1).length;
-        message.delete().catch();
-        message.channel.send('**Lista graczy w panelu (' + count + '):**' + send);
-    });
-    return;
-}
-
-if(cmd === '/help') {
-            let botmessage = args.join(" ");
-            message.delete().catch();
-            message.channel.send('```Komendy ogólnodostępne:\n/panel\n\nKomendy admina:\n/say [treść]\n/info [treść]```'); 
-        return;
-        }
-  });
+});
 
 bot.login(process.env.B0T_T0KEN);
